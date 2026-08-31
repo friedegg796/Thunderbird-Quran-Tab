48afac5bd28951886d1a847132130a62162ba91c
friedegg796 <hasnainalahiwork@gmail.com>
2026-08-31 18:37:13 -0400

    made w into details so it's more clear what it does

--- qurantab.js
+++ qurantab.js
@@ -7,13 +7,13 @@ browser.spacesToolbar.addButton('Quran', {
 });
  
 browser.webRequest.onBeforeSendHeaders.addListener(
-  function(w) {
-    w.requestHeaders.forEach(header => {
+  function(details) {
+    details.requestHeaders.forEach(header => {
       if (header.name.toLowerCase() === "user-agent") {
         header.value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/122.0";
       }
     });
-    return { requestHeaders: w.requestHeaders };
+    return { requestHeaders: details.requestHeaders };
   },
   { urls: ["https://quran.com/*"] },
   ["blocking", "requestHeaders"]
